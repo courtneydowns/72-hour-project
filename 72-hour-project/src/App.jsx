19b22adf7b53554819ea0f70ab425bf7aa73e030
Courtney Downs <courtneysdowns@gmail.com>
2021-08-25 18:26:05 -0400

    made changes to App.jsx and Ticketmaster.jsx

--- 72-hour-project/src/App.jsx
+++ 72-hour-project/src/App.jsx
@@ -6,13 +6,18 @@ function App() {
   const [location, setLocation] = useState({});
   const getLocation = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        setLocation(pos);
+      navigator.geolocation.getCurrentPosition((position) => {
+        // console.log(position);
+        setLocation(position);
       });
     }
   };
 
-  useEffect(getLocation, []);
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  // useEffect(getLocation, []); //empty array dependency (array) means the callback function is going to get called once when the component mounts
 
   return (
     <div>
--- 72-hour-project/src/components/Ticketmaster.jsx
+++ 72-hour-project/src/components/Ticketmaster.jsx
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from "react";
 
-const TicketmasterFetch = (props) => {
-  const url =
-    "https://app.ticketmaster.com/discovery/v2/events/latlong&apikey=OLGIKro3RShZ6AAJiMcpO7j1EIEHjiAF";
+const Ticketmaster = (props) => {
+  // const { latitude, longitude } = props.location.coords;
 
-  const [events, setEvents] = useState(null);
+  const [events, setEvents] = useState([]);
+
+  const key = "OLGIKro3RShZ6AAJiMcpO7j1EIEHjiAF";
+
+  const ticketmasterFetch = () => {
+    console.log(props.location);
+  };
+
+  const handlePropsChange = () => {
+    fetch(
+      `https://app.ticketmaster.com/discovery/v2/events.json?/latlong=${
+        props.location.coords.latitude + props.location.coords.longitude
+      }&apikey=${key}`
+    )
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setEvents(data.message));
+    handlePropsChange();
+    ticketmasterFetch();
   }, []);
 
   return (
     <div>
       <h1 className="Ticketmaster">Hello from Ticketmaster</h1>
-      <p>
-        {events.props.location.coords
-          ? events.props.location.coords.latitude
-          : null}
-      </p>
     </div>
   );
 };
 
-export default TicketmasterFetch;
-
-/*
-    1. Declare state variables for URL and the response
-    2. Write a function that will handle the fetch
-    3. Set everything up in order using useEffect hook ... set URL based on props (location (inside useEffect in dependency array..put props in))
-    4. Do Fetch...set response state setWHATEVER
-    5. Then display results however
-    */
+export default Ticketmaster;
