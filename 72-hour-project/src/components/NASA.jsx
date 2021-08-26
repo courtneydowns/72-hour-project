7bb5572169297af86f95d323603100db4fcbb8a9
kristenoom <kris.a.oom@gmail.com>
2021-08-26 07:58:43 -0400

    adding props for longitude and latitude

--- 72-hour-project/src/components/NASA.jsx
+++ 72-hour-project/src/components/NASA.jsx
@@ -1,15 +1,30 @@
-function NASA() {
+import React from 'react';
+
+const NASA = (props) => {
+
+    const {latitude, longitude} = props.location.coords;
+
     const baseURL = `https://api.nasa.gov/planetary/earth/imagery`;
     const key = `KTatox3u1ypx0L1rspOXjF9CjVu4Az1MHE0mdCbj`;
-    let longitude = 100.75;
-    let latitude = 1.5;
 
-    fetch(baseURL + '?lon=' + longitude + '?lat=' + latitude + '?api_key=' + key)
+    fetch(`${baseURL}?lon=${longitude}&lat=${latitude}&dim=0.10&api_key=${key}`)
     .then(response => response.json())
     .then(json => displayContent(json));
 
-    function displayContent(spaceObject) {
-        console.log(spaceObject);
+    function displayContent(spaceImg) {
+
+        console.log(spaceImg);
+
+        let image = document.createElement('img');
+        image.src = `${baseURL}?lon=${longitude}&lat=${latitude}&dim=0.10&api_key=${key}`;
+
+        //insert code here for where image will be displayed (e.g. card.insertBefore(image, cardBody);)
     }
+
+    return(
+        <div>
+            <img src={url} />
+        </div>
+    );
 }
 export default NASA;
