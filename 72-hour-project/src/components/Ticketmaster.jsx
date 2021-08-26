5e906eeab3c99014063fc3dff81f6f1f08d47bef
Courtney Downs <courtneysdowns@gmail.com>
2021-08-26 13:12:33 -0400

    react-strap update to package-lock.json and package.json and added event date and event distance to Ticketmaster.jsx

--- 72-hour-project/src/components/Ticketmaster.jsx
+++ 72-hour-project/src/components/Ticketmaster.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { Card, CardTitle, CardText, CardImg, CardImgOverlay } from "reactstrap";
 
 const Ticketmaster = (props) => {
   const [events, setEvents] = useState([]);
-
-  // const [handleClick, setHandleClick] = useState("");
 
   const key = "OLGIKro3RShZ6AAJiMcpO7j1EIEHjiAF";
 
@@ -14,14 +13,8 @@ const Ticketmaster = (props) => {
       .then((res) => res.json())
       .then((json) => {
         console.log(json);
+        console.log(json._embedded.events);
         setEvents(json._embedded.events);
-
-        // console.log(Array.from([json._embedded.events.name]));
-
-        // console.log(json._embedded.events.name);
-        // See if you can console log something specific from the response (maybe the first event)
-        //store the json in a useState Hook - setter function
-        // Try to only store the events array
       });
   };
 
@@ -32,17 +25,47 @@ const Ticketmaster = (props) => {
   }, [props.location.coords]);
   //use effect runs when component is mounted. WHen it's mounted, I am running my handlePropsChange immediately which is where my fetch is. Need to figure out way to do useEffect but not run fetch immediately...don't run fetch until button is clicked.
 
+  // function displayCards() {
+  //   {
+  //     events.map((event, id) => (
+  //       <p className="events" key={id}>
+  //         {event.name}
+  //       </p>
+  //     ));
+  //   }
+  // }
   return (
     <div>
       <h1 className="Ticketmaster">Ticketmaster: Nearby Events</h1>
       {events.map((event, id) => (
-        <p className="events" key={id}>
-          {event.name}
-          {/* {event.name.dates.start.localDate} */}
-        </p>
+        <div key={id}>
+          <h3 className="events">{event.name}</h3>
+          <p>{`Date: ${event.dates.start.localDate}`}</p>
+          <p>{`Distance: ${event.distance}`}</p>
+        </div>
       ))}
     </div>
   );
 };
+
+// const Example = (props) => {
+//   return (
+//     <div>
+//       <Card inverse>
+//         <CardImg width="100%" src="/assets/318x270.svg" alt="Card image cap" />
+//         <CardImgOverlay>
+//           <CardTitle tag="h5">Card Title</CardTitle>
+//           <CardText>
+//             This is a wider card with supporting text below as a natural lead-in
+//             to additional content. This content is a little bit longer.
+//           </CardText>
+//           <CardText>
+//             <small className="text-muted">Last updated 3 mins ago</small>
+//           </CardText>
+//         </CardImgOverlay>
+//       </Card>
+//     </div>
+//   );
+// };
 
 export default Ticketmaster;
