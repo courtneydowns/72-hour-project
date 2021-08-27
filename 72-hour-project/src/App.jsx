820e15da57ed232a88a6fb5587076c5a2ba6cc63
kristenoom <kris.a.oom@gmail.com>
2021-08-27 19:50:07 -0400

    app.jsx modification to prevent stylization breaking

--- 72-hour-project/src/App.jsx
+++ 72-hour-project/src/App.jsx
@@ -28,32 +28,13 @@ function App() {
   return (
     <div className="App">
       <center>
-        <h1>OUR API FORM</h1>
-      {/* <form>
-        <label for="latitude">Latitude</label><br />
-        <input type="number" id="" name="latitude" /><br />
-        <label for="longitude">Longitude</label><br />
-        <input type="number" id="" name="longitude" />
-        <br />
-        <button className="btn btn-primary" onClick={getLocation}>SUBMIT</button>
-      </form> */}
-      <table width="600">
-        <tbody>
-        <tr>
-          <td>
+      
             <NASA location={location} />
-          </td>
-          <td>
+          
             <Weather />
-          </td>
-        </tr>
-        <tr>
-          <td colSpan="2">
+
             <Ticketmaster location={location} />
-          </td>
-        </tr>
-        </tbody>
-      </table>
+
       </center>
     </div>
   );
